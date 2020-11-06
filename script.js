e7ac9eea0cd88cb31db865b8e5bffdc571fea756
Arina <a.konshina.job@gmail.com>
2020-11-06 10:09:43 +0100

    Это мой первый комит

---
+++ script.js
@@ -0,0 +1,52 @@
+// 'use strict';
+a = 5;
+console.log(a);
+
+var leftBorferWidth = 1;
+let second = 2;
+const pi = 3.14;
+console.log(4/0);
+console.log('lala'*5)
+let smth;
+console.log(smth);
+
+let persone = {
+    name: "Jone",
+    age: 25,
+    isMarred: false
+}
+
+console.log(persone["name"]);
+
+let arr = ['pulm.png', 'orange.jpeg', 'apple.bmp'];
+
+console.log(arr[0]);
+
+// alert("hello world");
+
+// let answer = confirm("Are you here?");
+// console.log(answer);
+
+// let answer = +prompt("Are you here?", "yes");
+// console.log(typeof(answer));
+
+//  console.log("arr" + "=");
+// console.log(4 + +" - orrors");
+
+let incr = 10,
+    decr = 10;
+
+
+
+console.log(incr++);
+console.log(decr--);
+
+
+console.log(5%2);
+console.log("2" == 2);
+console.log("2" === 2);
+
+let isChecked = true,
+    isClosed = false;
+
+console.log(isChecked && !isClosed);
